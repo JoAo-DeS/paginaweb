eedad9d3891e05944f2e3d31f5b8c8fab659b0c1
João.exe <joaocisielskids@gmail.com>
2026-08-06 22:15:18 -0300

    botao

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector('Button');
+botao.addEventListener('Click', botaoclicado);
+
+function botaoclicado() {
+    console.log("Clicado");
+    let texto = botao.querySelector("span");
+    texto.textContent++;
+}
